970c40be9cd9e6751aa58f553abb084164128216
Arjoo <2009arjoo@gmail.com>
2022-04-15 23:10:41 +0530

    added changes

---
+++ employee-service/src/main/java/com/arjoo/employee/service/EmployeeService.java
@@ -0,0 +1,46 @@
+package com.arjoo.employee.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.arjoo.employee.entity.Department;
+import com.arjoo.employee.entity.Employee;
+import com.arjoo.employee.entity.ResponseWrapper;
+import com.arjoo.employee.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	private static Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private RestTemplate template;
+
+	public Employee saveEmployee(Employee employee) {
+		logger.info("Inside saveEmployee of EmployeeService");
+		return employeeRepository.save(employee);
+	}
+
+	public ResponseWrapper findEmployee(Long id) {
+		logger.info("Inside findEmployee of EmployeeService");
+		Department department = template.getForObject("http://DEPARTMENT-SERVICE/departments/" + id, Department.class);
+		Employee emp = new Employee();
+		emp.setEmployeeId(id);
+		emp.setEmployeeEmail("200@gmail.com");
+		emp.setEmployeeFirstName("Arjoo");
+		emp.setEmployeeLastName("Kumar");
+		emp.setDepartmentId(1l);
+
+		ResponseWrapper wrapper = new ResponseWrapper();
+		wrapper.setDepartment(department);
+		wrapper.setEmployee(emp);
+		
+		return wrapper;
+	}
+}
