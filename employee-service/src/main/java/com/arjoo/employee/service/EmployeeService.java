b522016b6d4066445dc155456e948ea08267d002
arjoo <2009arjoo@gmail.com>
2024-09-20 16:26:39 +0530

    Added Dockerfile in eureka server

--- employee-service/src/main/java/com/arjoo/employee/service/EmployeeService.java
+++ employee-service/src/main/java/com/arjoo/employee/service/EmployeeService.java
@@ -1,15 +1,14 @@
 package com.arjoo.employee.service;
 
+import com.arjoo.employee.entity.Department;
+import com.arjoo.employee.entity.Employee;
+import com.arjoo.employee.entity.ResponseWrapper;
+import com.arjoo.employee.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import com.arjoo.employee.entity.Department;
-import com.arjoo.employee.entity.Employee;
-import com.arjoo.employee.entity.ResponseWrapper;
-import com.arjoo.employee.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
